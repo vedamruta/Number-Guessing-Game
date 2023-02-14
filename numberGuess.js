6ccb7c5e5f6f4e1ff544a7df64cadf2c25dd3df3
Vedamruta <vedamruta2153@gmail.com>
2023-02-14 13:37:21 +0530

    REwriting js code(added event handler)

--- numberGuess.js
+++ numberGuess.js
@@ -1,36 +1,54 @@
-// Step 1 : 
-// Generate a random number between 1 and 100.
-let r_no = Math.floor(Math.random()*100) + 1;
-// 'random()' method generates a random decimal between 0(inclusive) and 1(exclusive).
-// 1 is excluded. 
-// Step 2 : 
-// Record the turn number the player is on.
-const turn_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let notification = document.getElementsByClassName('notification'); 
-let near_far = document.getElementsByClassName('near-far');
+const txtno1 = document.getElementById('txt_number');
+        const button = document.getElementById('submit_btn');
+        const output = document.getElementById('previous_guess');
 
-document.getElementById('restart').ariaDisabled = true;
+        function funct() {
+            output.innerHTML += " "+txtno1.value;
+        }
+        button.addEventListener('click', funct);
 
-for (let i=0; i<turn_array.length; i++) {
-    const guessed_no = document.getElementById('txt_number');
-    let previousguesses = document.querySelector('span');
-    previousguesses.innerText += ` ${r_no}`;
-    if (guessed_no === r_no) {
-        notification.innerText = "Congratulations! You got it right!";
-        document.getElementById('txt_number').ariaDisabled = true;
-        document.getElementById('submit_btn').ariaDisabled = true;
-        document.getElementById('restart').ariaDisabled = false;
-        break;
-    }
-    else if (guessed_no > r_no){
-        notification.innerText = "Wrong!";
-        near_far.innerText = `Last guess was too high!`;
-    }
-    else if (guessed_no < r_no) {
-        notification.innerText = "Wrong!";
-        near_far.innerText = `Last guess was too low!`;
-    }
+        function guess_game() {
+            // We generate a random number from 1 to 100
+            const random = Math.floor(Math.random()*100) + 1;
 
-}
-notification.innerText = "!!!GAME OVER!!!";
-document.getElementById('restart').ariaDisabled = false;
+            // We assign a counter since there only 10 attempts allowed in total
+            let count = 1;
+
+            const notification = document.getElementById('notification');
+            const near_far = document.getElementById('near-far');
+            const restart = document.getElementById('restart');
+
+            //LOGIC Begins
+            // Disable visibility of 'Start new game' button
+            restart.disabled = true;
+
+            while(count<=10) {
+                if(txtno1===random) {
+                    notification.innerHTML = "Congratulations!! Correct.";
+                    button.disabled = true;
+                    txtno1.disabled = true;
+                    restart.disabled = false;
+                    near_far.innerHTML = "";
+                }   
+                else if(txtno1!==random && count<=9) {
+                    notification.innerHTML = "Incorrect !!";
+                    if(txtno1<random) {
+                        near_far.innerHTML = "Very Low.";
+                    }
+                    else if (txtno1>random) {
+                        near_far.innerHTML = "Very High.";
+                    }
+                }
+                else if(txtno1!==random && count==10) {
+                    notification.innerHTML = "Incorrect !!";
+                    near_far.innerHTML = "Game Over!";
+                    restart.disabled = false;
+                    button.disabled = true;
+                    txtno1.disabled = true;
+                    // console.log(count);
+                }
+                count++;
+                // console.log(count);
+            }
+        }
+        // button.addEventListener('click', guess_game);
