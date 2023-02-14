d9730bc64c9ddd2ae40d7d09fd2ab115fe8cf40f
Vedamruta <vedamruta2153@gmail.com>
2023-02-14 15:16:48 +0530

    Hurray!! Game is Working :)

--- numberGuess.js
+++ numberGuess.js
@@ -1,54 +1,59 @@
 const txtno1 = document.getElementById('txt_number');
-        const button = document.getElementById('submit_btn');
-        const output = document.getElementById('previous_guess');
+const button = document.getElementById('submit_btn');
+const output = document.getElementById('previous_guess');
+const notification = document.getElementById('notification');
+const near_far = document.getElementById('near-far');
+const restart = document.getElementById('restart');
 
-        function funct() {
-            output.innerHTML += " "+txtno1.value;
-        }
-        button.addEventListener('click', funct);
+function submit_click() {
+  output.innerHTML += " "+txtno1.value;
+}
+button.addEventListener('click', submit_click);
 
-        function guess_game() {
-            // We generate a random number from 1 to 100
-            const random = Math.floor(Math.random()*100) + 1;
+// We generate a random number from 1 to 100
+const random = Math.floor(Math.random()*100) + 1;
 
-            // We assign a counter since there only 10 attempts allowed in total
-            let count = 1;
+// We assign a counter since there only 10 attempts allowed in total
+let count = 0;
 
-            const notification = document.getElementById('notification');
-            const near_far = document.getElementById('near-far');
-            const restart = document.getElementById('restart');
+function guess_game() {
+//LOGIC Begins
+// Disable visibility of 'Start new game' button
+  restart.disabled = true;
+  count++;
+  if(count<=10) {
+    if(txtno1.value==random) {
+      // if(txtno1===random) here, `===` doesn't work because here input is a number and no strict comparison is required.
+      notification.innerHTML = "Congratulations!! Correct.";
+      button.disabled = true;
+      txtno1.disabled = true;
+      restart.disabled = false;
+      near_far.innerHTML = "";
+    }   
+    else if(txtno1.value<random) {
+      notification.innerHTML = "Incorrect !!";
+      near_far.innerHTML = "Very Low.";
+    }
+    else if (txtno1.value>random) {
+      notification.innerHTML = "Incorrect !!";
+      near_far.innerHTML = "Very High.";
+    }
+    if(count===10 && txtno1.value!==random) {
+      notification.innerHTML = "Incorrect !!";
+      near_far.innerHTML = "Game Over!";
+      restart.disabled = false;
+      button.disabled = true;
+      txtno1.disabled = true;
+    }
+  }
+}
+button.addEventListener('click', guess_game);
 
-            //LOGIC Begins
-            // Disable visibility of 'Start new game' button
-            restart.disabled = true;
-
-            while(count<=10) {
-                if(txtno1===random) {
-                    notification.innerHTML = "Congratulations!! Correct.";
-                    button.disabled = true;
-                    txtno1.disabled = true;
-                    restart.disabled = false;
-                    near_far.innerHTML = "";
-                }   
-                else if(txtno1!==random && count<=9) {
-                    notification.innerHTML = "Incorrect !!";
-                    if(txtno1<random) {
-                        near_far.innerHTML = "Very Low.";
-                    }
-                    else if (txtno1>random) {
-                        near_far.innerHTML = "Very High.";
-                    }
-                }
-                else if(txtno1!==random && count==10) {
-                    notification.innerHTML = "Incorrect !!";
-                    near_far.innerHTML = "Game Over!";
-                    restart.disabled = false;
-                    button.disabled = true;
-                    txtno1.disabled = true;
-                    // console.log(count);
-                }
-                count++;
-                // console.log(count);
-            }
-        }
-        // button.addEventListener('click', guess_game);
+function restart_click() {
+  txtno1.disabled = false;
+  txtno1.value = "";
+  button.disabled = false;
+  count = 0;
+  output.innerHTML = "";
+}
+restart.addEventListener('click', restart_click);
